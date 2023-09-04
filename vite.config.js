64c782b4b32e3ee4c5255067bcac7d02d2cd1f1b
ajncarz <cardona.aljun.csucc@gmail.com>
2023-09-05 03:46:40 +0800

    okay

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
 
-  plugins: [react()],
+  if (command !== 'serve') {
+    config.base = '/MyProjectDaisy/'
+  }
+
+  return config
 })
